8f05517db7b77641ba1b6397e8ec7c140e97102d
Tim Olsen <tolsen@limelabs.com>
2008-02-02 00:22:36 +0000

    add Credentials::load() and Credentials::dump_sans_creds()
    git-svn-id: file:///srv/svn/limedav/better_httpauth/trunk@88 5302b642-3916-44d6-a90b-748a43cdb162

--- lib/httpauth/digest.rb
+++ lib/httpauth/digest.rb
@@ -292,6 +292,14 @@ module HTTPAuth
         credentials.update_from_challenge! options
         credentials
       end
+
+      def self.load(filename, options={})
+        h = nil
+        File.open(filename, 'r') do |f|
+          h = Marshal.load f
+        end
+        new h, options
+      end
       
       # Create a new instance.
       #
@@ -388,6 +396,13 @@ module HTTPAuth
         end
         @h[:response] = Utils.calculate_digest(@h, @s, :request)
       end
+
+      def dump_sans_creds(filename)
+        File.open(filename, 'w') do |f|
+          Marshal.dump(Utils.filter_h_on(@h, [:username, :realm, :nonce, :algorithm, :cnonce, :opaque, :qop, :nc]), f)
+        end
+      end
+      
     end
     
     # The Challenge class handlers the WWW-Authenticate header. The WWW-Authenticate header is sent by a server when
